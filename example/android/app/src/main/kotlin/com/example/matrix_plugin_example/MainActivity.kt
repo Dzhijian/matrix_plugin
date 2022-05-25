d33cc683d362a02f68748c8dc3a33d714c0fce3b
James <646724452@qq.com>
2022-05-25 17:53:19 +0800

    matrix sdk flutter plugin

---
+++ example/android/app/src/main/kotlin/com/example/matrix_plugin_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.matrix_plugin_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ios/Classes/MatrixPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface MatrixPlugin : NSObject<FlutterPlugin>
+@end
